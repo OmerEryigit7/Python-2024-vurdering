9c86a7bb895e1b5253e5d317e0c00049bd41fec3
kjetil <kjrea003@osloskolen.no>
2024-10-14 12:32:04 +0200

    legge til varer(ikke klar)

---
+++ ny_varer.py
@@ -0,0 +1,24 @@
+#!/bin/python
+import tkinter as tk
+def ny_vare():
+    root = tk.Tk()
+    root.title("ny vare")
+    def ting():
+         text_widget = tk.Text(root)
+         text_widget.pack(padx=10, pady=10, fill="both")
+         pris= tk.Button(root, text="leg til prisen", command=ting)
+         pris.pack(pady=10)
+    
+    text_widget = tk.Text(root)
+    text_widget.pack(padx=10, pady=10, fill="both")
+
+    ny_vare= tk.Button(root, text="leg til ny vare", command=ting)
+    ny_vare.pack(pady=10)
+
+    status_label = tk.Label(root, text="", padx=1, pady=1)
+    status_label.pack()
+
+
+
+    root.mainloop()
+ny_vare()
